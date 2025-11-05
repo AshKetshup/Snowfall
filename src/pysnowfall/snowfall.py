7c500708eb2a519a192ca7cc08600894474eca8e
AshKetshup <dsimoes2000@gmail.com>
2025-11-05 22:08:43 +0000

    Fixes minor import problem
    Uses relative import paths.

--- src/pysnowfall/snowfall.py
+++ src/pysnowfall/snowfall.py
@@ -1,16 +1,20 @@
-from snowfallConfig import SnowfallConfig
 from datetime import datetime, timezone
 
 from helpers import generateTimestamp, waitUntilNextTimestamp
 
+from .snowfallConfig import SnowfallConfig
+
 
 class ErrorSettingTimestampException(Exception):
     """Exception describing an error while setting a timestamp."""
-    EXCEPTION_MESSAGE : str =  "CLOCK IS RUNNING BACKWARDS"
+
+    EXCEPTION_MESSAGE: str = "CLOCK IS RUNNING BACKWARDS"
+
     def __init__(self, message: str = EXCEPTION_MESSAGE) -> None:
         self.message = message
 
-class Snowfall():
+
+class Snowfall:
     """
     class Snowfall for generation of snowflakes
 
@@ -18,14 +22,14 @@ class Snowfall():
         ErrorSettingTimestampException: If the system clock is moving backwards.
     """
 
-    SHIFT_TIME      : int = 10 + 12
-    SHIFT_GENERATOR : int = 10
+    SHIFT_TIME: int = 10 + 12
+    SHIFT_GENERATOR: int = 10
 
-    Configuration   : SnowfallConfig = SnowfallConfig()
+    Configuration: SnowfallConfig = SnowfallConfig()
 
-    _lastTimestamp  : int | None = None
-    _maxSequence    : int = (1 << Configuration.MACHINE_SEQUENCE_BITS) - 1
-    _sequence       : int = 0
+    _lastTimestamp: int | None = None
+    _maxSequence: int = (1 << Configuration.MACHINE_SEQUENCE_BITS) - 1
+    _sequence: int = 0
 
     @classmethod
     def generateSnowflake(cls, date: datetime = datetime.now(timezone.utc)) -> int:
@@ -47,23 +51,23 @@ class Snowfall():
         Returns:
             int: The generated snowflake ID.
         """
-        currentConfig : SnowfallConfig = cls.Configuration
-        timestamp     : int = generateTimestamp(date)
+        currentConfig: SnowfallConfig = cls.Configuration
+        timestamp: int = generateTimestamp(date)
 
         if cls._lastTimestamp and timestamp < cls._lastTimestamp:
             raise ErrorSettingTimestampException()
-        
+
         if timestamp == cls._lastTimestamp:
             cls._sequence = (cls._sequence + 1) & cls._maxSequence
             if not cls._sequence:
                 timestamp = waitUntilNextTimestamp(timestamp)
         else:
             cls._sequence = 0
-            
+
         cls._lastTimestamp = timestamp
 
         return (
-            ( ( timestamp - currentConfig.Epoch ) << cls.SHIFT_TIME ) 
-            | ( currentConfig.WorkedID << cls.SHIFT_GENERATOR ) 
+            ((timestamp - currentConfig.Epoch) << cls.SHIFT_TIME)
+            | (currentConfig.WorkedID << cls.SHIFT_GENERATOR)
             | cls._sequence
         )
--- src/pysnowfall/snowflake.py
+++ src/pysnowfall/snowflake.py
@@ -1,18 +1,19 @@
 from datetime import datetime, timezone
 
-from snowfallConfig import SnowfallConfig
-from helpers        import getMask
+from .helpers import getMask
+from .snowfallConfig import SnowfallConfig
 
 
-class Snowflake():
+class Snowflake:
     """Snowflake assembler class"""
 
-    _maskGenerator : int = getMask(10)
-    _maskSequence  : int = getMask(12)
-    _shiftTime     : int = 22
+    _maskGenerator: int = getMask(10)
+    _maskSequence: int = getMask(12)
+    _shiftTime: int = 22
 
-    
-    def __init__(self, snowflakeID: int | str, configuration: SnowfallConfig = SnowfallConfig()) -> None: 
+    def __init__(
+        self, snowflakeID: int | str, configuration: SnowfallConfig = SnowfallConfig()
+    ) -> None:
         """
         Initializes a new instance using a snowflake ID and an optional configuration.
 
@@ -20,11 +21,11 @@ class Snowflake():
             snowflakeID (int | str): The snowflake ID as a `integer` or `string`.
             configuration (SnowfallConfig, optional): The configuration to use for creating the instance. Defaults to SnowfallConfig().
         """
-        self.SnowflakeID    : int            = int(snowflakeID)
-        self.Timestamp      : int            = 0
-        self.MachineID      : int            = 0
-        self.Sequence       : int            = 0
-        self._configuration : SnowfallConfig = configuration
+        self.SnowflakeID: int = int(snowflakeID)
+        self.Timestamp: int = 0
+        self.MachineID: int = 0
+        self.Sequence: int = 0
+        self._configuration: SnowfallConfig = configuration
         self._decodeSnowflake(self.SnowflakeID)
 
     @property
@@ -34,9 +35,8 @@ class Snowflake():
 
         Returns:
             datetime: a `datetime` object representing the timestamp.
-        """        
+        """
         return self._timestampToDatetime()
-    
 
     def updateConfiguration(self, configuration: SnowfallConfig) -> None:
         """
@@ -44,7 +44,7 @@ class Snowflake():
 
         Args:
             configuration (SnowfallConfig): The new configuration to set.
-        """        
+        """
         self._configuration = configuration
         self._decodeSnowflake(self.SnowflakeID)
 
@@ -56,9 +56,10 @@ class Snowflake():
             snowflake (int): The Snowflake ID to decode.
         """
         self.Timestamp = (snowflake >> self._shiftTime) + self._configuration.Epoch
-        self.MachineID = (snowflake >> SnowfallConfig.MACHINE_SEQUENCE_BITS) & self._maskGenerator
-        self.Sequence  = snowflake & self._maskSequence
-
+        self.MachineID = (
+            snowflake >> SnowfallConfig.MACHINE_SEQUENCE_BITS
+        ) & self._maskGenerator
+        self.Sequence = snowflake & self._maskSequence
 
     def _timestampToDatetime(self) -> datetime:
         """
@@ -67,4 +68,4 @@ class Snowflake():
         Returns:
             datetime: a `datetime` object representing the timestamp.
         """
-        return datetime.fromtimestamp( self.Timestamp / (10**3), tz=timezone.utc )
+        return datetime.fromtimestamp(self.Timestamp / (10**3), tz=timezone.utc)
